bf8e2aabb6d9d4eb71169152ddf17d29e8631c6e
iOSDeveloperGui <guilhermesilvadias535@gmail.com>
2025-10-02 20:25:22 -0300

    Initial Commit

---
+++ WaterClock/WaterClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterClockApp.swift
+//  WaterClock
+//
+//  Created by iOS Developer on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
